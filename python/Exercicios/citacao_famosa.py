89882d51bc502849aa9ca945e35e5242a8e9ce0e
Deyved Antonio <deyved.antonio@gmail.com>
2023-11-07 21:59:51 -0300

    feat: exercícios para praticar.

---
+++ python/Exercicios/citacao_famosa.py
@@ -0,0 +1,3 @@
+citacao = 'Uma pessoa que nunca cometeu um erro jamais tentou nada novo.'
+autor = 'Albert Einstein'
+print(f'{autor} certa vez disse: "{citacao}"')
---
+++ python/Exercicios/letras_nomes.py
@@ -0,0 +1,4 @@
+nome = 'Deyved Antonio'
+print(f'Nome maiúsculo: {nome.upper()}')
+print(f'Nome minúsculo: {nome.lower()}')
+print(f'Nome 1° maiúscula: {nome.title()}')
