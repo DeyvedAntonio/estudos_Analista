d6ed29438582972c1ae800cfb9b24d339e2a0b1a
Deyved Antonio <deyved.antonio@gmail.com>
2023-11-07 22:17:12 -0300

    feat: exercícios para praticar

---
+++ python/Exercicios/numero_oito.py
@@ -0,0 +1,11 @@
+# adição
+print(f'Adição: 6 + 2 = {6+2}')
+
+# subtração
+print(f'Subtração: 9 - 1 = {9-1}')
+
+# divisão
+print(f'Divisão: 24 / 3 = {24//3}')
+
+# multiplicação
+print(f'Multiplicação: 4 * 2 = {4*2}')
---
+++ python/Exercicios/removendo_caracteres.py
@@ -0,0 +1,8 @@
+nome = '\tDeyved Antonio     '
+print(f'O nome é:{nome}.')
+nome_rstrip = nome.rstrip()
+print(f'Remover espaços no final:{nome_rstrip}.')
+nome_lstrip = nome.lstrip()
+print(f'Remover espaços do início:{nome_lstrip}.')
+nome_strip = nome.strip()
+print(f'Sem espaços:{nome_strip}.')
