8234e74c3e18688206e7deeb816de6819d135ea3
Deyved Antonio <deyved.antonio@gmail.com>
2023-11-17 18:55:17 -0300

    feat: exibir_forca, exibir_palavra e jogo_forca.

--- python/curso_dsa/Cap07/projeto_1.py
+++ python/curso_dsa/Cap07/projeto_1.py
@@ -10,10 +10,25 @@ def escolher_palavra():
         return random.choice(conteudo.split('\n'))
 
 def exibir_forca(erros):
-    pass
+    if erros == 0:
+        print('''
+    _____
+    |   |
+    |   
+    |
+    |   
+    -----''')
+    elif erros == 1:
+        pass
 
 def exibir_palavra(palavra, letras_corretas):
-    pass
+    for letra in palavra:
+        if letra == ' ':
+            print(' ', end=' ')
+        elif letra in letras_corretas:
+            print(f'{letra}', end=' ')
+        else:
+            print('_', end=' ')
 
 def jogo_forca():
     palavra_escolhida = escolher_palavra()
@@ -22,8 +37,29 @@ def jogo_forca():
     tentativas_restantes = 6
 
     while tentativas_restantes > 0:
-        exibir_forca(len(letras_incorretas))
         exibir_palavra(palavra_escolhida, letras_corretas)
-
+        print(f'\n\nChances restantes: {tentativas_restantes}')
+        print(f'Letras erradas: {[letras_incorretas[index] for index, letra in enumerate(letras_incorretas)]}\n')
+        
+        try:
+            tentativa = str(input('Digite uma letra: '))
+        except TypeError:
+            print('Opção inválida! Tente novamente.')
+        
+        if tentativa in palavra_escolhida:
+            letras_corretas.append(tentativa)
+            # exibir_palavra(palavra_escolhida, letras_corretas)
+            if len(letras_corretas) == len(palavra_escolhida):
+                print(f'\nVocê venceu, a palavra é : {palavra_escolhida}')
+        else:
+            letras_incorretas.append(tentativa)
+            tentativas_restantes -= 1
+            exibir_forca(len(letras_incorretas))
+    
     if tentativas_restantes == 0:
-        print(f'Você perdeu! A palavra era {palavra_escolhida}.')
+        print(f'\n\nVocê perdeu! A palavra era {palavra_escolhida}.')
+
+if __name__ == '__main__':
+    print('Bem-vindo(a) ao jogo da forca!\nAdivinhe a palavra abaixo:\n')
+    jogo_forca()
+    print(f'\nParabéns. Você está aprendendo programação em Python com a DSA.')
