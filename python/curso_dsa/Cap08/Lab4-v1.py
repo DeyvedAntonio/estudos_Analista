0eca3f1d3afcbefe75575765ba903ac299dadf4f
Deyved Antonio <deyved.antonio@gmail.com>
2023-11-22 21:44:30 -0300

    feat: funções acabou_jogo, status_jogo.

--- python/curso_dsa/Cap08/Lab4-v1.py
+++ python/curso_dsa/Cap08/Lab4-v1.py
@@ -72,6 +72,7 @@ class Hangman:
 	# Método Construtor
      def __init__(self) -> None:
           lista_palavras = []
+          
           try:
                with open(r'Cap07\arquivos\lista_palavras.txt', 'r', encoding='utf-8') as arquivo:
                     lista_palavras = arquivo.read()
@@ -89,17 +90,30 @@ class Hangman:
           tentativas = 6
           letras_erradas = []
           letras_tentativas = []
-          while tentativas > 0:
+          letras_certas = []
+          
+          while tentativas != 0:
                print(f'\n\nChances restantes: {tentativas}')
                print(f'Letras erradas: {letras_erradas[:]}')
                letra = input('\nDigite uma letra: ')
                if letra not in palavra_sorteada:
                     letras_erradas.append(letra)
+                    letras_tentativas.append(letra)
                     tentativas -= 1
-	
+               else:
+                    letras_tentativas.append(letra)
+                    letras_certas.append(letra)
+               
+               if tentativas != 0:
+                    self.status_jogo(palavra_sorteada, letras_certas, len(letras_erradas))
+               
+               self.jogador_venceu()
+               self.acabou_jogo(tentativas, palavra_sorteada)
+
 	# Método para verificar se o jogo terminou
-     def acabou_jogo(self):
-          pass
+     def acabou_jogo(self, tentativas, palavra):
+          if tentativas == 0:
+               print(f'\n{board[-1]}\n\nVocê perdeu! A palavra correta é {palavra}.\n')
 		
 	# Método para verificar se o jogador venceu
      def jogador_venceu(self):
@@ -125,8 +139,16 @@ class Hangman:
           return palavra_escolhida
 		
 	# Método para checar o status do game e imprimir o board na tela
-     def status_jogo(self):
-          pass
+     def status_jogo(self, palavra: str, certas: list, tamanho: int) -> None:
+          print(f'\n{board[tamanho]}\n')
+          for letra in palavra:
+               if letra == ' ':
+                    print(' ', end=' ')
+               elif letra in certas:
+                    print(letra, end=' ')
+               else:
+                    print('_', end=' ')
+
 
 if __name__ == '__main__':
      jogo = Hangman()
