51fd8f5de64e193bd0391c1a1dd5ed576976c6c1
Deyved Antonio <deyved.antonio@gmail.com>
2023-11-24 21:12:40 -0300

    refactor: nudança nas funções.

--- python/curso_dsa/Cap08/Lab4-v1.py
+++ python/curso_dsa/Cap08/Lab4-v1.py
@@ -82,7 +82,7 @@ class Hangman:
           print('Seja Bem-vindo(a)')
           print(f'{board[0]}\n')
           
-          palavra = self.esconder_letra(lista_palavras)
+          palavra = random.choice(lista_palavras.split('\n'))
           self.advinhar_letra(palavra)
 
 	# Método para adivinhar a letra
@@ -90,9 +90,12 @@ class Hangman:
           tentativas = 6
           letras_erradas = []
           letras_tentativas = []
+          letras_unicas = set(palavra_sorteada)
           letras_certas = []
-          
-          while tentativas != 0:
+          self.esconder_letra(palavra_sorteada)
+          fim_jogo = False
+
+          while fim_jogo == False:
                print(f'\n\nChances restantes: {tentativas}')
                print(f'Letras erradas: {letras_erradas[:]}')
                letra = input('\nDigite uma letra: ')
@@ -107,47 +110,41 @@ class Hangman:
                if tentativas != 0:
                     self.status_jogo(palavra_sorteada, letras_certas, len(letras_erradas))
                
-               self.jogador_venceu()
-               self.acabou_jogo(tentativas, palavra_sorteada)
+               fim_jogo = self.jogador_venceu(letras_unicas, letras_certas)
+               fim_jogo = self.acabou_jogo(tentativas, palavra_sorteada)
 
 	# Método para verificar se o jogo terminou
      def acabou_jogo(self, tentativas, palavra):
           if tentativas == 0:
                print(f'\n{board[-1]}\n\nVocê perdeu! A palavra correta é {palavra}.\n')
+               return True
+          else:
+               return False
 		
 	# Método para verificar se o jogador venceu
-     def jogador_venceu(self):
-          pass
+     def jogador_venceu(self, conjunto_unico, conjunto_certas):
+          if len(conjunto_certas) == len(conjunto_unico):
+               print('\nParabéns você venceu!')
+               return True
+          else:
+               return False
 		
 	# Método para não mostrar a letra no board
-     def esconder_letra(self, conteudo: list) -> str:
-          """
-          Escolhe uma palavra de uma lista de palavras e exibir _ no lugar das letras.
-
-          Args:
-              conteudo (list): Coleção de palavras.
-
-          Returns:
-              str: Retorna uma palavra escolhida de uma lista. 
-          """
-          palavra_escolhida = random.choice(conteudo.split('\n'))
-          for letra in palavra_escolhida:
+     def esconder_letra(self, texto: str) -> None:
+          for letra in texto:
                if letra == ' ':
                     print(' ', end=' ')
                else:
                     print('_', end=' ')
-          return palavra_escolhida
 		
 	# Método para checar o status do game e imprimir o board na tela
      def status_jogo(self, palavra: str, certas: list, tamanho: int) -> None:
           print(f'\n{board[tamanho]}\n')
           for letra in palavra:
-               if letra == ' ':
-                    print(' ', end=' ')
-               elif letra in certas:
+               if letra in certas:
                     print(letra, end=' ')
                else:
-                    print('_', end=' ')
+                    self.esconder_letra(letra)
 
 
 if __name__ == '__main__':
