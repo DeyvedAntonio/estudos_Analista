980b9f850ff8c9a2854135bb4502957a5d21b4d8
Deyved Antonio <deyved.antonio@gmail.com>
2026-01-24 00:26:43 +0000

    feat: resolução de exercício 05

---
+++ python/desenvolvimento_web/semana_04_python_basio/exercicios/exe05.py
@@ -0,0 +1,11 @@
+# Exercício 5 — Soma manual
+# 	Crie uma lista com dois números.
+# 	Calcule a soma manualmente e informe se o resultado é maior que 10.
+# 	Objetivo:
+#         Operadores + lista.
+
+lista = [32, 45]
+
+soma = lista[0] + lista[1]
+
+print(f'O resultado é {soma}.')
