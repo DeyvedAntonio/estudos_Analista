3d9e232908438e784a096c8a045262d72d1feddc
Deyved Antonio <deyved.antonio@gmail.com>
2026-01-24 00:26:15 +0000

    feat: resolução de exercício 01

---
+++ python/desenvolvimento_web/semana_04_python_basio/exercicios/exe01.py
@@ -0,0 +1,9 @@
+# Exercício 1 — Acesso por índice
+# 	Crie uma lista com três números.
+# 	Imprima o primeiro e o último número da lista.
+# 	Objetivo:
+#         Entender indexação básica.
+
+lista_numeros = [10, 23, 45, 70, 11, 3, 15, 7, 9, 30]
+print(f'O primeiro é {lista_numeros[0]}')
+print(f'O últime é {lista_numeros[-1]}')
