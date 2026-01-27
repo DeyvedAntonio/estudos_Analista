f86464e6dabe8226aa86522bd6929dc123bc0976
Deyved Antonio <deyved.antonio@gmail.com>
2026-01-26 21:49:15 -0300

    refactor: mudança no método de avaliar risco.

--- python/desenvolvimento_web/semana_06_POO/analise_credito.py
+++ python/desenvolvimento_web/semana_06_POO/analise_credito.py
@@ -1,29 +1,21 @@
-from datetime import datetime
-
-
 class AnaliseCredito:
 
-    def __init__(self, nome_cliente, renda_mensal, valor_solicitado, score_credito, status_analise='Em Análise') -> None:    # NOQA: E501
-        self.identificador = f'{datetime.now()}{nome_cliente[0:5]}{valor_solicitado}'  # NOQA: E501
+    def __init__(self, nome_cliente, renda_mensal, valor_solicitado, score_credito, status_analise='Em Análise') -> None:  # NOQA: E501
+        self.identificador = f'{renda_mensal}{valor_solicitado}{score_credito}'  # NOQA: E501
         self.nome_cliente = nome_cliente
         self.renda_mensal = renda_mensal
         self.valor_solicitado = valor_solicitado
         self.score_credito = score_credito
         self.status_analise = status_analise
 
-    def avaliar_risco(
-            self,
-            renda_mensal,
-            valor_solicitado,
-            score_credito
-    ) -> str:
+    def avaliar_risco(self) -> str:
 
-        if valor_solicitado > (renda_mensal * 0.3):
-            return 'Reprovado'
-        elif score_credito <= 600:
-            return 'Reprovado'
+        if self.valor_solicitado > (self.renda_mensal * 0.3):
+            return f'Caro, {self.nome_cliente} sua solicitação {self.identificador} foi Reprovada.'  # NOQA: E501
+        elif self.score_credito <= 600:
+            return f'Caro, {self.nome_cliente} sua solicitação {self.identificador} foi Reprovada.'  # NOQA: E501
         else:
-            return 'Aprovado'
+            return f'Caro, {self.nome_cliente} sua solicitação {self.identificador} foi Aprovada.'  # NOQA: E501
 
 
 if __name__ == '__main__':
@@ -31,6 +23,6 @@ if __name__ == '__main__':
     analise_3 = AnaliseCredito('José Maria', 5_000.00, 1_500.00, 800)
     analise_2 = AnaliseCredito('Maria', 7_000.00, 2_500.00, 550)
 
-    print(analise_1.avaliar_risco(5_000.00, 2_500.00, 800))
-    print(analise_3.avaliar_risco(5_000.00, 1_500.00, 800))
-    print(analise_2.avaliar_risco(7_000.00, 2_500.00, 550))
+    print(analise_1.avaliar_risco())
+    print(analise_3.avaliar_risco())
+    print(analise_2.avaliar_risco())
