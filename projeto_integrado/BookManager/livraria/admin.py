fab3493236ed6e3bee6c7c843bc8102f69ca8929
Deyved Antonio <deyved.antonio@gmail.com>
2026-02-16 16:40:27 -0300

    feat: criação dos models das classes de `Autor` e `Livro`.

--- projeto_integrado/BookManager/livraria/admin.py
+++ projeto_integrado/BookManager/livraria/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+from django.contrib import admin                               # type: ignore
+from .models import Autor, Livro
 
-# Register your models here.
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    fields = ['nome', 'nacionalidade', 'data_nascimento']
+    search_fields = ['nome', 'nacionalidade']
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    fields = ['titulo', 'preco', 'data_publicacao', 'autor']
+    search_fields = ['titulo', 'autor']
--- projeto_integrado/BookManager/livraria/models.py
+++ projeto_integrado/BookManager/livraria/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Autor(models.Model):
+
+    nome = models.CharField(
+        max_length=150,
+        verbose_name='Author',
+    )
+    nacionalidade = models.CharField(
+        max_length=150,
+        verbose_name='Nacionalidade',
+    )
+    data_nascimento = models.DateField(
+        verbose_name='Data de Nascimento'
+    )
+    ativo = models.BooleanField(
+        verbose_name='Ativo',
+        default=True
+    )
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Livro(models.Model):
+
+    titulo = models.CharField(
+        max_length=200,
+        verbose_name='Título do Livro'
+    )
+    preco = models.FloatField(
+        verbose_name='Preço do Livro',
+        blank=True,
+        null=True,
+    )
+    data_publicacao = models.DateField(
+        verbose_name='Data de Publicação',
+        blank=True,
+        null=True,
+    )
+    autor = models.ForeignKey(
+        Autor,
+        on_delete=models.PROTECT,
+        related_name='fk_livro_autor',
+        verbose_name='Autor do Livro',
+    )
+
+    def __str__(self) -> str:
+        return f'{self.titulo} - {self.autor}'
