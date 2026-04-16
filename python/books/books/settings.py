0515535c15ffbd46066c8f68ed269af5f8ab8f5f
Deyved Antonio <deyved.antonio@gmail.com>
2026-04-16 17:18:22 -0300

    docs: migrate do banco de dados.

--- python/books/books/settings.py
+++ python/books/books/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "categorias",
-    "livros",
     "autores",
+    "categorias",
+    "emprestimos",
+    "livros",
+    "membros",
 ]
 
 MIDDLEWARE = [
