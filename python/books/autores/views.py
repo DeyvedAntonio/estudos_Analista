68361067a02f560c16452e75f34a416584198de4
Deyved Antonio <deyved.antonio@gmail.com>
2026-04-17 08:48:08 -0300

    feat: início da criação da view do app autores.

--- python/books/autores/views.py
+++ python/books/autores/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from .models import Autor
 
-# Create your views here.
+
+class AutorListView(ListView):
+    model = Autor
+    template_name = 'autores_list.html'
+
+
+class AutorDetailView(DetailView):
+    model = Autor
+    template_name = 'autores_detail.html'
+
+
+class AutorCreateView(CreateView):
+    model = Autor
+    template_name = 'autores_create.html'
+    success_url = ''
+    context_object_name = 'autores'
+
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = 'autores_delete.html'
+    success_url = ''
