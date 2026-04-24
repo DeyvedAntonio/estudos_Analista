edafc680bd73b46760b190f4d32652b433f9567a
Deyved Antonio <deyved.antonio@gmail.com>
2026-04-24 20:02:59 -0300

    feat: news views.

--- python/Django/djangotutorial/polls/views.py
+++ python/Django/djangotutorial/polls/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('Hello, world. You are at the polls index.')
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    return render(request, f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return render(request, response % question_id)
+
+
+def vote(request, question_id):
+    return render(request, f"You're voting on question {question_id}")
