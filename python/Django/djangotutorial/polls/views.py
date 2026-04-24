eb91dd07a79b327e92859ec2346f579ee203d545
Deyved Antonio <deyved.antonio@gmail.com>
2026-04-24 20:48:25 -0300

    feat: error handling 404.

--- python/Django/djangotutorial/polls/views.py
+++ python/Django/djangotutorial/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -10,7 +11,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return render(request, f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
